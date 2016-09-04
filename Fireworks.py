3c9b6e1f8cb8ffe76d3cb28b4a8f4a1b412f80fd
eholweg <severe.wx@gmail.com>
2016-09-04 15:23:56 -0400

    First set of edits to project

--- Fireworks.py
+++ Fireworks.py
@@ -12,7 +12,8 @@ class Fireworks:
         fcstPattern=re.compile('FCST,.*')
 
         fullObsDict={}
-        obsDict={};
+        #obsDict={}
+        fullFcstDict={}
 
         #READ IN THE OBS FILE
         f = open(FWOMRX, 'r')
@@ -22,7 +23,7 @@ class Fireworks:
             matchOb=re.match(obPattern, ob)
             if matchOb:
                 #print "MATCH -- "+ob.rstrip()
-                obList=string.split(ob.rstrip());
+                obList=ob.rstrip().split()
 
                 obsDict={"sta_name": obList[0],
                          "date": obList[2],
@@ -52,35 +53,35 @@ class Fireworks:
         fwm = fm.readlines()
 
         for fcst in fwm:
-            matchOb = re.match(fcstPattern, fcst)
-            if matchOb:
-                # print "MATCH -- "+ob.rstrip()
-                obList = string.split(ob.rstrip());
+            matchFcst = re.match(fcstPattern, fcst)
+            if matchFcst:
+                print fcst
 
-                obsDict = {"sta_name": obList[0],
-                           "date": obList[2],
-                           "hr": obList[3],
-                           "t": obList[4],
-                           "w": obList[5],
-                           "dbt": obList[6],
-                           "dpt": obList[7],
-                           "rh": obList[8],
-                           "y": obList[9],
-                           "m": obList[10],
-                           "dir": obList[11],
-                           "ws": obList[12],
-                           "10": obList[13],
-                           "tmx": obList[14],
-                           "tmn": obList[15],
-                           "hmx": obList[16],
-                           "hmn": obList[17],
-                           "pd1": obList[18],
-                           "pd2": obList[19]
+                fcstList = fcst.rstrip().split(',')
+
+                fcstDict = {"sta_id": fcstList[1],
+                           "date": fcstList[2],
+                           "hr": fcstList[3],
+                           "wx": fcstList[4],
+                           "temp": fcstList[5],
+                           "rh": fcstList[6],
+                           "lal1423": fcstList[7],
+                           "lal2323": fcstList[8],
+                           "wdir": fcstList[9],
+                           "wspd": fcstList[10],
+                           "tenhr": fcstList[11],
+                           "maxtemp": fcstList[12],
+                           "mintemp": fcstList[13],
+                           "maxrh": fcstList[14],
+                           "minrh": fcstList[15],
+                           "pd1305": fcstList[16],
+                           "pd0513": fcstList[17],
+                           "wetflag": fcstList[18]
                            }
-                fullObsDict[obList[1]] = obsDict
+                fullFcstDict[fcstList[1]] = fcstDict
 
-        for k in fullObsDict:
-            print k, fullObsDict[k]['tmx']
+        for k in fullFcstDict:
+            print k, fullFcstDict[k]['maxtemp']
 
 
 
